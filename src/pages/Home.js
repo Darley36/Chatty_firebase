03086afd2ea283b76bf011adb468fff35bd6a13c
Darley36 <darleybest@gmail.com>
2021-10-17 03:15:54 -0500

    the project works, some tests are missing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div>
       <div className="home-app">
-        <Header></Header>
         <section>
-          <img src="https://www.sofka.com.co/wp-content/uploads/2021/02/sofkau-logo-horizontal.png" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-xQRVP9WZGXHvBl3mTjuygGbcFpJk-5G4VA&usqp=CAU" />
           <div className="jumbotron jumbotron-fluid py-5">
             <div className="container text-center py-5">
               <h1 className="display-4">Bienvenido al chat grupal</h1>
@@ -17,6 +17,7 @@ export default function Home() {
                 <Link className="boton-crear" to="/signup">
                   Crear Cuenta
                 </Link>
+                <br/>
                 <Link className="boton-crear" to="/login">
                   Iniciar sesion
                 </Link>
@@ -24,7 +25,6 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <Footer></Footer>
       </div>
     </div>
   );
