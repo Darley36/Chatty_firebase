0a77cac09e3e256130b54152fad6b774022dd0c8
Darley36 <darleybest@gmail.com>
2021-10-17 02:56:41 -0500

    HomePage modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <div>
+      <div className="home-app">
+        <Header></Header>
+        <section>
+          <img src="https://www.sofka.com.co/wp-content/uploads/2021/02/sofkau-logo-horizontal.png" />
+          <div className="jumbotron jumbotron-fluid py-5">
+            <div className="container text-center py-5">
+              <h1 className="display-4">Bienvenido al chat grupal</h1>
+              <p className="lead">
+                Diviertete hablando con tus amigos
+              </p>
+              <div className="mt-4">
+                <Link className="boton-crear" to="/signup">
+                  Crear Cuenta
+                </Link>
+                <Link className="boton-crear" to="/login">
+                  Iniciar sesion
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 }
